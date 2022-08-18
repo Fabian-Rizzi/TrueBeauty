f6c18074f4f0a2c024dedbda9b220f9bb8847dbe
fabelin <82421798+fabelin@users.noreply.github.com>
2022-08-18 18:08:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const splashi = document.querySelector('.splashscreen');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+    setTimeout(()=>{
+        splashi.classList.add('display-none');
+    }, 1000);
+})
+
+function passwordToggle() {
+    let campo = document.getElementsByClassName("logInPasswordInput");
+    if (campo[0].type == "text") {
+        campo[0].type = "password";
+    } else {
+    campo[0].type = "text";
+    }
+}
+
+const logBtn = document.getElementById("logBtn");
+logBtn.addEventListener('click', function logBtnFn() {
+    logBtn.style.transition = "all 0.1s ease-in-out"
+    logBtn.style.backgroundColor = "#A6739D";  
+    setTimeout(() => {
+        logBtn.style.backgroundColor = "#E2B0DA";  
+      }, 100);
+
+      setTimeout(() => {
+        let emailInput = document.getElementsByClassName("logInEmailInput");
+      let passwordInput = document.getElementsByClassName("logInPasswordInput")
+      if (emailInput[0].value === "lena92@gmail.com" && passwordInput[0].value === "lena92") {
+          console.log("datos correctos");
+          window.location.href = '/home.html';
+      } else {
+          console.log("datos incorrectos");
+      }
+      }, 800)
+});
+
+var innerScreenHeight = window.innerHeight;
+document.getElementById("homeTabBar").style.top = innerScreenHeight + "755px";
+
+let homeSearch = document.getElementById("homeSearchInput");
+
+// homeSearch.addEventListener('focusin', function hideNavBar() {
+//    document.getElementById("homeTabBar").addClass('hide-footer');
+//    console.log("asd");
+// }); 
+    
+// homeSearch.onfocus = (ev) => {
+//    document.getElementById("homeTabBar").addClass('hide-footer');
+//    console.log("asd");
+// };
